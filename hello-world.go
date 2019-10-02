e86b57bf2739d9e942e9f33d5a09ea454a5d1e56
Ubuntu <ubuntu@ip-172-31-19-92.ap-southeast-1.compute.internal>
2019-10-02 05:19:36 +0000

    Test2

--- hello-world.go
+++ hello-world.go
@@ -11,7 +11,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Hello world received a request.")
 	target := os.Getenv("TARGET")
 	if target == "" {
-		target = "World"
+		target = "Bill World"
 	}
 	fmt.Fprintf(w, "Hello %s!", target)
 }
